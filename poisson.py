b6e8a50c533a25683e68752923b091b2156d9f9d
anjalafghan <anjal.afghan93@gmail.com>
2018-06-30 20:36:02 +0530

    critical update 1.0

--- poisson.py
+++ poisson.py
@@ -1,17 +1,22 @@
-lambdap = float(input("Value of lambda?"))
-r = float(input("value of r in P(X=r)"))
+import math
 
-def poisson(r):
-	
-	return 
+lambdap = 0.3
+r = 0
 
-def probability( r ):
-		if r <= 0:
-			return lambdap/r+1
-		else:
-			return (lambdap/r+1)*probability(r-1)
-		
-probability(r)
+
+def probability(r):
+    if r <= 0:
+        return math.pow(lambdap, -r) * (math.exp(-lambdap)/ math.factorial(r))
+    else:
+
+        return (lambdap/r+1) * probability(r-1)
+
+
+ for lambdap in :
+     print(probability(r))
+
+#print(probability(r))
+
 
 
 
